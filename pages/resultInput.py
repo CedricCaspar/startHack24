fa0005c0f20348d7a3266e69ae94c3ed8a301c16
ccaspar <cedricmcaspar@gmail.com>
2024-03-21 16:20:55 +0100

    result input

--- pages/resultInput.py
+++ pages/resultInput.py
@@ -1,10 +1,146 @@
 import streamlit as st
 
-from src.sg_files.globalData import getExam
+from data import Result
+from src.sg_files.globalData import data
 
-exam = getExam(0)
+objectives = data["objectives"]
+
+if 'data' not in st.session_state:
+    st.session_state["data"] = data
+if 'examId' not in st.session_state:
+    st.session_state['examId'] = 0
+if 'studentId' not in st.session_state:
+    st.session_state['studentId'] = 0
+if 'classId' not in st.session_state:
+    st.session_state['classId'] = 0
+formats = {1: "Not reached", 1.5: "", 2: "reached", 2.5: "", 3: "well reached", 3.5: "", 4: "very well reached"}
+selectedResults = []
 
 
-st.title("" + exam.subject + ", " + exam.creation)
+@st.cache_resource
+def getResult(eId, sId, oId):
+    for i in range(len(st.session_state.data["results"])):
+        if st.session_state.data["results"][i].examId == eId and st.session_state.data["results"][i].studentId == sId and st.session_state.data["results"][
+            i].objectiveId == oId:
+            return st.session_state.data["results"][i]
+    st.session_state.data["results"].append(Result(sId, eId, oId, 1))
+    return st.session_state.data["results"][-1]
+
+
+def setResult(eId, sId, oId, value):
+    for i in range(len(st.session_state.data["results"])):
+        if st.session_state.data["results"][i].examId == eId and st.session_state.data["results"][i].studentId == sId and st.session_state.data["results"][
+            i].objectiveId == oId:
+            st.session_state.data["results"][i].value = value
+            return st.session_state.data["results"][i]
+
+
+def format_labels(l):
+    return formats[l]
+
+
+def nextStudent():
+    for i, question in enumerate(st.session_state.data["exams"][st.session_state.examId].objectives):
+        setResult(st.session_state.examId, st.session_state.studentId,
+                  question.objectiveId, selectedResults[i])
+    st.session_state.studentId += 1
+    print("Saved: ", st.session_state.data["results"])
+
+
+def prevStudent():
+    for i, question in enumerate(st.session_state.data["exams"][st.session_state.examId].objectives):
+        setResult(st.session_state.examId, st.session_state.studentId,
+                  question.objectiveId, selectedResults[i])
+    st.session_state.studentId -= 1
+    print("Saved: ", st.session_state.data["results"])
+
+
+st.set_page_config(layout='wide')
+
+st.title("" + st.session_state.data["exams"][st.session_state.examId].subject + ", " + st.session_state.data["exams"][
+    st.session_state.examId].creation)
 st.divider()
-st.button("Previous")
+
+st.markdown(
+    """
+    <style>
+        div[data-testid="column"]:nth-of-type(1)
+        {
+        } 
+
+        div[data-testid="column"]:nth-of-type(2)
+        {
+            text-align: center;
+        }
+        div[data-testid="column"]:nth-of-type(3)
+        {
+            text-align: end;
+        } 
+    </style>
+    """, unsafe_allow_html=True
+)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.button("Previous", disabled=(st.session_state.studentId == 0), on_click=prevStudent)
+
+with col2:
+    st.markdown("### " + st.session_state.data["classes"][st.session_state.classId][st.session_state.studentId].name)
+with col3:
+    st.button("Next", disabled=(
+            st.session_state.studentId == len(st.session_state.data["classes"][st.session_state.classId]) - 1),
+              on_click=nextStudent)
+st.text("")
+col4, col5 = st.columns([8, 4])
+with col4:
+    st.markdown("## Lernziele")
+with col5:
+    st.markdown(
+        """
+        <style>
+            div[data-testid="column"]:nth-of-type(1)
+            {
+            } 
+
+            div[data-testid="column"]:nth-of-type(2)
+            {
+                text-align: left;
+            }
+            div[data-testid="column"]:nth-of-type(3)
+            {
+                text_align: left
+            }
+            div[data-testid="column"]:nth-of-type(4)
+            {
+                text-align: end;
+            } 
+        </style>
+        """, unsafe_allow_html=True
+    )
+    col6, col7, col8, col9 = st.columns(4)
+    with col6:
+        """Not Reached"""
+    with col7:
+        """Reached"""
+    with col8:
+        """Well Reached"""
+    with col9:
+        """Excellent"""
+for question in st.session_state.data["exams"][st.session_state.examId].objectives:
+    with st.container():
+        col4, col5 = st.columns([8, 4])
+        with col4:
+            st.markdown(f"#### {objectives[question.objectiveId].name}")
+            st.markdown(f"{objectives[question.objectiveId].desc}")
+        with col5:
+            v = getResult(st.session_state.examId,
+                          st.session_state.studentId, question.objectiveId).value
+            selected = st.select_slider("label",
+                                        [1, 1.5, 2, 2.5, 3, 3.5, 4],
+                                        key=question.number,
+                                        label_visibility="hidden",
+                                        value=v,
+                                        # format_func=format_labels,
+                                        )
+            print("Retrieved: ", v)
+            selectedResults.append(selected)
--- src/sg_files/globalData.py
+++ src/sg_files/globalData.py
@@ -1,15 +1,55 @@
-from data import Objective, Exam, Student
+from data import Objective, Exam, Student, Result
 
-objectives: dict = {0: Objective(0, "Ich kann Flächeninhalte schätzen", "Mathematik"),
-                    1: Objective(1, "Ich kann Fläche, Umfang & Seitenlange von Rechtecken bestimmen", "Mathematik"),
-                    2: Objective(2, "Ich kann Flächenmasse umwandeln", "Mathematik"),
-                    3: Objective(3, "Ich kann Aussagen zu einem Sachtext mit richtig oder falsch beurteilen", "Deutsch"),
-                    4: Objective(4, "Ich kann in Sätzen Kommas korrekt setzen", "Deutsch"),
-                    5: Objective(5, "Ich kann in Sätzen Bindewörter passend einsetzen", "Deutsch"),
-                    6: Objective(6, "Ich setze Rechtschreibung und Grammatik korrekt um", "Deutsch")}
+data = {"objectives": {0: Objective(0, "Ich kann Flächeninhalte schätzen.", "Mathematik"),
+                    1: Objective(1, "Ich kann Fläche, Umfang & Seitenlange von Rechtecken bestimmen.", "Mathematik"),
+                    2: Objective(2, "Ich kann Flächenmasse umwandeln.", "Mathematik"),
+                    3: Objective(3, "Ich kann Aussagen zu einem Sachtext mit richtig oder falsch beurteilen.",
+                                 "Deutsch"),
+                    4: Objective(4, "Ich kann in Sätzen Kommas korrekt setzen.", "Deutsch"),
+                    5: Objective(5, "Ich kann in Sätzen Bindewörter passend einsetzen.", "Deutsch"),
+                    6: Objective(6, "Ich setze Rechtschreibung und Grammatik korrekt um.", "Deutsch")},
+        "exams": [Exam(0, "17.04.24", "Deutsch", ["1", "1-2", "3"], [4, 6, 5], [0.25, 0.5, 0.25])],
+        "classes": [[Student(0, "Alex"), Student(1, "Naomi")]],
+        "results": []}
 
-exams = [Exam(0, "17.04.24", "Deutsch", ["1", "1-2", "3"], [4, 6, 5], [0.25, 0.5, 0.25])]
-classe = [Student]
+
+def printResults():
+    print(data["results"])
+
+def getResults():
+    return data["results"]
+
 
 def getExam(id):
-    return exams[id]
+    return data["exams"][id]
+
+
+def getClasse():
+    return data["classes"][0]
+
+
+def getObjectives():
+    return data["objectives"]
+
+
+def getResultId(data, eId, sId, oId):
+    for i in range(len(data["results"])):
+        if data["results"][i].examId == eId and data["results"][i].studentId == sId and data["results"][i].objectiveId == oId:
+            return i
+    data["results"].append(Result(sId, eId, oId, None))
+    return len(data["results"])-1
+
+
+def getResult(data, eId, sId, oId):
+    for i in range(len(data["results"])):
+        if data["results"][i].examId == eId and data["results"][i].studentId == sId and data["results"][i].objectiveId == oId:
+            return data["results"][i]
+    data["results"].append(Result(sId, eId, oId, None))
+    return data["results"][-1]
+
+
+def setResult(data, eId, sId, oId, value):
+    for i in range(len(data["results"])):
+        if data["results"][i].examId == eId and data["results"][i].studentId == sId and data["results"][i].objectiveId == oId:
+            data["results"][i].value = value
+            return data["results"][i]
